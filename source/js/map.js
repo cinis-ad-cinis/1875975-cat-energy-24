3080b4de6c7f0b834f36d0c10ae43bdc833efb24
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2022-01-26 20:53:01 +0300

    Правки после первой проверки

---
+++ source/js/map.js
@@ -0,0 +1,5 @@
+let iframeMap = document.querySelector('.map__iframe');
+let imageMap = document.querySelector('.map__image');
+
+iframeMap.classList.remove('map__iframe--nojs');
+imageMap.classList.add('map__image--hide')

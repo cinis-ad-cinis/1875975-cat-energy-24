d3b69c8a04613a5c4e169a26520645bdee423c6c
cinis-ad-cinis <88879570+cinis-ad-cinis@users.noreply.github.com>
2022-06-05 22:54:07 +0000

    Deploying to gh-pages from @ cinis-ad-cinis/1875975-cat-energy-24@e6ce2267d982b78801724aa75e3a3fc90311a658 🚀

---
+++ js/map.js
@@ -0,0 +1,1 @@
+let iframeMap=document.querySelector(".map__iframe"),imageMap=document.querySelector(".map__image");iframeMap.classList.remove("map__iframe--nojs"),imageMap.classList.add("map__image--hide");
--- source/js/map.js
+++
@@ -1,5 +0,0 @@
-let iframeMap = document.querySelector('.map__iframe');
-let imageMap = document.querySelector('.map__image');
-
-iframeMap.classList.remove('map__iframe--nojs');
-imageMap.classList.add('map__image--hide')
